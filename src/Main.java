e270caaf83efc4cf135d3d690fc11d6d8db991e3
Anik Ghosh <ldexter0910@gmail.com>
2022-02-02 23:59:43 +0530

    initialise

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    // Main class to call instances of subclasses and work accordingly
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
